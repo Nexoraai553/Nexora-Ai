27e92f6bd507eb5d666f65948606151c3e2223d2
Nexoraai553 <nexoraai553@gmail.com>
2026-07-19 01:08:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-document.querySelectorAll("button").forEach((button, index) => {
-  button.addEventListener("click", () => {
-    if(index === 0){
-      window.location.href = "summarizer.html";
-    } else {
-      alert("This AI tool is coming soon!");
-    }
-  });
+let currentLang = "en";
+
+document.getElementById("langBtn").addEventListener("click", function () {
+
+  if (currentLang === "en") {
+    currentLang = "ar";
+    this.innerText = "English";
+  } else {
+    currentLang = "en";
+    this.innerText = "العربية";
+  }
+
+  document.getElementById("title").innerText =
+    translations[currentLang].title;
+
+  document.getElementById("description").innerText =
+    translations[currentLang].description;
+
+  document.getElementById("summarizer").innerText =
+    translations[currentLang].summarizer;
+
+  document.getElementById("email").innerText =
+    translations[currentLang].email;
+
+  document.getElementById("blog").innerText =
+    translations[currentLang].blog;
+
+  document.getElementById("resume").innerText =
+    translations[currentLang].resume;
+
+  document.getElementById("translator").innerText =
+    translations[currentLang].translator;
+
 });
+
+
+function openSummarizer(){
+  window.location.href = "summarizer.html";
+}
