3a05552d1ba3c5b1c08ec250eecea07c2f1ee3e8
Nexoraai553 <nexoraai553@gmail.com>
2026-07-19 01:49:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-alert("Script is working!");
+let currentLang = "en";
 
-document.getElementById("langBtn").onclick = function(){
+const btn = document.getElementById("langBtn");
 
-alert("Button works!");
+btn.onclick = function () {
+
+  if (currentLang === "en") {
+
+    currentLang = "ar";
+    btn.innerText = "English";
+
+    document.querySelector(".logo").innerText = "⚡ كويك AI";
+
+    document.querySelector("header h1").innerText =
+    "مساعدك الذكي لكل شيء";
+
+    document.querySelector("header p").innerText =
+    "أنشئ واكتب وطوّر أعمالك باستخدام أدوات الذكاء الاصطناعي.";
+
+  } else {
+
+    currentLang = "en";
+    btn.innerText = "العربية";
+
+    document.querySelector(".logo").innerText =
+    "⚡ QuickAI";
+
+    document.querySelector("header h1").innerText =
+    "Your AI Assistant For Everything";
+
+    document.querySelector("header p").innerText =
+    "Create, write and improve your work with powerful AI tools.";
+
+  }
 
 };
+
+
+function openSummarizer(){
+
+window.location.href = "summarizer.html";
+
+}
