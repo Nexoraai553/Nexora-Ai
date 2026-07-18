4b2216e514ec91391bc8b75ae30bef5f760c2689
Nexoraai553 <nexoraai553@gmail.com>
2026-07-19 00:45:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+document.querySelectorAll("button").forEach((button, index) => {
+  button.addEventListener("click", () => {
+    if(index === 0){
+      window.location.href = "summarizer.html";
+    } else {
+      alert("This AI tool is coming soon!");
+    }
+  });
+});
